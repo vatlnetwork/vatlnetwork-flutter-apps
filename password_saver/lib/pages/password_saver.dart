2012c1d02db81257aded8bc125fb2af38e6153eb
John Gustafson <john.gustafson@ddst.dev>
2023-07-05 17:20:23 -0600

    added a log out button

--- password_saver/lib/pages/password_saver.dart
+++ password_saver/lib/pages/password_saver.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:password_saver/controllers/account.dart';
+
 class PasswordSaver extends StatefulWidget {
   final Function setPage;
 
@@ -10,10 +12,36 @@ class PasswordSaver extends StatefulWidget {
 }
 
 class _PasswordSaverState extends State<PasswordSaver> {
+  final accountController = AccountController();
+
+  final ThemeData redTheme = ThemeData.from(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+    useMaterial3: true
+  );
+
+  logOut() async {
+    await accountController.deleteCurrentToken();
+    widget.setPage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Password Saver')
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(),
+              ElevatedButton(
+                onPressed: () { logOut(); },
+                child: const Text('Log Out', style: TextStyle(color: Colors.red))
+              )
+            ]
+          )
+        ]
+      ),
     );
   }
 }
